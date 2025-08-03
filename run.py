b45fcda541359834170fbaedb2edc61104314f02
noise <rcy13141314@gmail.com>
2025-08-03 23:41:14 +0800

    优化

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8888,
+        port=9999,
         reload=False,
         log_config="uvicorn_loggin_config.json",
     )
